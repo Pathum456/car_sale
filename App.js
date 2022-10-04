ef21dcc71a89f5bd9b598a94893a61c4b031879c
pathum456 <gdse58courseonlinelessons2021@gmail.com>
2022-10-04 11:46:06 +0530

    image Upload from Gallery Done

--- App.js
+++ App.js
@@ -4,11 +4,13 @@ import {createStackNavigator} from '@react-navigation/stack';
 import Home from './components/screen/Home';
 import LoginPage from './components/screen/LoginPage';
 import RegisterFormPage from './components/screen/RegisterFormPage';
+import AddCar from './components/screen/AddCar';
+
 const Stack = createStackNavigator();
 export default function App() {
   return (
     <View>
-      <RegisterFormPage />
+      <AddCar />
     </View>
   );
 }
--- components/screen/AddCar.js
+++ components/screen/AddCar.js
@@ -1,12 +1,78 @@
-import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import {Alert, StyleSheet, PermissionsAndroid} from 'react-native';
+import React, {useState} from 'react';
+import {
+  NativeBaseProvider,
+  Text,
+  Input,
+  VStack,
+  Button,
+  View,
+  Heading,
+  HStack,
+  Image,
+} from 'native-base';
+import {launchImageLibrary} from 'react-native-image-picker';
+
+//launchCamera(options, callback);
+
+// You can also use as a promise without 'callback':
+//const result = await launchCamera(options);
 
 export default function AddCar() {
+  //const [cameraPhoto, setCameraPhoto] = useState();
+  const [galleryPhoto, setGalleryPhoto] = useState();
+
+  let options = {
+    saveToPhotos: true,
+    mediaType: 'photo,',
+  };
+  /*const openCamera = async () => {
+    const granted = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.CAMERA,
+    );
+    if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+      const result = await launchCamera(options);
+      setCameraPhoto(result.assets[0].uri);
+    }
+  };*/
+  const openGallery = async () => {
+    const result = await launchImageLibrary(options);
+    setGalleryPhoto(result.assets[0].uri);
+    console.log(result.assets[0]);
+  };
+
   return (
-    <View>
-      <Text>AddCar</Text>
-    </View>
+    <NativeBaseProvider>
+      <View style={styles.view_set}>
+        <VStack space={4} alignItems="center">
+          <Heading fontSize="3xl" textAlign="center" ml="5%" mt="5%">
+            Add Car Details
+          </Heading>
+          <Image
+            size={300}
+            source={{uri: galleryPhoto}}
+            alt="Alternate Text"
+            mt="5%"
+          />
+          <Button
+            size="md"
+            variant="subtle"
+            bg="primary.300"
+            w={'32'}
+            shadow={5}
+            onPress={openGallery}>
+            Upload Image
+          </Button>
+        </VStack>
+      </View>
+    </NativeBaseProvider>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  view_set: {
+    backgroundColor: '#F9E7FE',
+    width: '100%',
+    height: 690,
+  },
+});
